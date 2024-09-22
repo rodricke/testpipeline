dee15fcd915b52af522f2097ffe76cdb08573a7d
Ali Ahsan <ali.ahsan@xgrid.co>
2024-09-22 15:33:50 +0500

    added code and yml file

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string version = "1.0.0";
+        Console.WriteLine($"Hello World version {version}");
+    }
+}
